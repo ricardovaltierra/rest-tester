622a820725cecebed90e2900a5555ee4d61ae8ad
ClintonEnyinna <enyinnaclinton@gmail.com>
2020-02-04 11:15:14 -0600

    feat: add actions and views for user resource routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def index
+    @name = "I am the Index action!"
+  end
+
+  def new
+    @name = "I am the New action!"
+  end
+
+  def edit
+    @name = "I am the Edit action!"
+  end
+
+  def show
+    @name = "I am the Show action!"
+  end
 end
